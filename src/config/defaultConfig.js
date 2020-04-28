d631e327c55e1f35c4cb9783d3d74650b9281179
weishunjie-github <157680333@qq.com>
2020-04-28 17:24:52 +0800

    wsj

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   hostname:'127.0.0.1',
-  port:9527,
+  port:9528,
   root:process.cwd()//用户当前路径
 }
---
+++ src/helper/route.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const promisify = require('util').promisify;
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
+module.exports = async function (req, res, filePath) {
+  try {
+    const stats = stat(filePath)
+    if (stats.isFile()) {
+      res.StatusCode = 200;
+      res.setHeader("Content-Type", "text/plain")
+      fs.createReadStream(filePath).pipe(res)
+    } else if (stats.isDirectory()) {
+      const files = readdir(filePath);
+      res.StatusCode = 200;
+      res.setHeader("Content-Type", "text/plain")
+      res.end(files.join(','));
+    }
+  } catch (ex) {
+    res.StatusCode = 404;
+    res.setHeader("content-Type", "text/plain");
+    res.end(`${filePath} is not a directory or file`);
+  }
+}
