61fa0e5e0161035acc865095f7e0773400d7a160
weishunjie-github <157680333@qq.com>
2020-06-11 13:21:22 +0800

    618 优化

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const conf = require('./config/defaultConfig')
 const path = require("path")
 const route = require('./helper/route')
 const server = http.createServer((req, res) => {
+  
   const filePath = path.join(conf.root, req.url)
   route(req,res,filePath);
 })
