ec3d5e14d78aa3d0f2cb8ad15f4a7824a85ad141
weishunjie-github <157680333@qq.com>
2020-06-15 17:47:30 +0800

    wsj

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # node-anydoor
 tiny nodejs static web server
+
+
+## 安裝
+
+、、、
+
+npm i -g anydoor
+、、、
+
 ## 使用方法
+、、、
+anydoor # 把当前文件夹当做静态资源服务器根目录
+
+andoor -p 8080  #手工自定义端口号
+
+anydoor -h localhost #设置 host为localhost
+
+anydoor -d /user #设置根目录为/user
+、、、
--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-const http = require("http");
-const chalk = require("chalk")
-const conf = require('./config/defaultConfig')
-const path = require("path")
-const route = require('./helper/route')
-const server = http.createServer((req, res) => {
-  console.info(req.url)
-  const filePath = path.join(conf.root, req.url)
-  route(req,res,filePath);
-})
-server.listen(conf.port, conf.hostname, () => {
-  
-  const addr = `http://${conf.hostname}:${conf.port}`;
-  console.info(`Server started at ${chalk.green(addr)}`);
-});
+// const http = require("http");
+// const chalk = require("chalk")
+// const conf = require('./config/defaultConfig')
+// const path = require("path")
+// const route = require('./helper/route')
+// class Server {
+//   constructor(config) {
+//     this.conf = Object.aasin({}, conf, config)
+//   }
+//   start() {
+//     const server = http.createServer((req, res) => {
+//       console.info(req.url)
+//       const filePath = path.join(conf.root, req.url)
+//       route(req, res, filePath);
+//     })
+//     server.listen(conf.port, conf.hostname, () => {
+
+//       const addr = `http://${conf.hostname}:${conf.port}`;
+//       console.info(`Server started at ${chalk.green(addr)}`);
+//     });
+//   }
+// }
